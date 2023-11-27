f823d8075924193db3da542e7052e3e4a4db02b5
Chris McComb <ccmcc2012@gmail.com>
2023-11-27 15:06:08 -0500

    Adding tests

---
+++ tests/test.py
@@ -0,0 +1,45 @@
+import unittest
+import scipy.optimize
+from SAE.fmincon import car, weightsNull, generate_feasible
+
+
+class TestOptimization(unittest.TestCase):
+
+    def test_fmincon(self):
+
+        def round_x(x):
+            for i in range(19, len(x)):
+                rounded = round(x[i])
+                x[i] = rounded
+            return x
+
+        def objective(x):
+            c = car()
+            c.set_vec(round_x(x))
+            return c.objectives(weightsNull, with_subobjs=False)
+
+        def penalty_1(x):
+            c = car()
+            c.set_vec(round_x(x))
+            return c.constraints_bound()
+
+        def penalty_2(x):
+            c = car()
+            c.set_vec(round_x(x))
+            return c.constraints_nonlin_ineq()
+
+        def penalty_3(x):
+            c = car()
+            c.set_vec(round_x(x))
+            return c.constraints_lin_ineq()
+
+        scipy.optimize.minimize(
+            objective,
+            generate_feasible().vector,
+            method='trust-constr',
+            constraints=(
+                {'type': 'ineq', 'fun': penalty_1},
+                {'type': 'ineq', 'fun': penalty_2},
+                {'type': 'ineq', 'fun': penalty_3}
+             )
+        )
